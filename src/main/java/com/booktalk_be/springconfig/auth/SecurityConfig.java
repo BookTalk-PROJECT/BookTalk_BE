de8b32be1b700404d74c6747e4a50ffcc3a3eb33
HyeongSeok-Choi <qnftlstm@naver.com>
2025-06-13 01:57:01 +0900

    add jwt provider
    jwt 작업중

---
+++ src/main/java/com/booktalk_be/springconfig/auth/SecurityConfig.java
@@ -0,0 +1,69 @@
+package com.booktalk_be.springconfig.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
+        return new MvcRequestMatcher.Builder(introspector);
+    }
+
+
+    @Bean
+    public SecurityFilterChain springSecurityFilterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+        MvcRequestMatcher.Builder mvc = new MvcRequestMatcher.Builder(introspector);
+
+        // white list (Spring Security 체크 제외 목록)
+        MvcRequestMatcher[] permitAllList = {
+                mvc.pattern("/login"),
+                mvc.pattern("/register"),
+                mvc.pattern("/token-refresh"),
+                mvc.pattern("/favicon.ico"),
+                mvc.pattern("/error")
+        };
+
+        http.authorizeHttpRequests(authorize -> authorize
+                .requestMatchers(permitAllList).permitAll()
+//                        .requestMatchers(HttpMethod.DELETE, "/user").hasRole(RoleName.ROLE_ADMIN.getRole())
+//                        // 그 외 요청 체크
+                        .anyRequest().authenticated()
+        );
+
+        // form login disable
+        http.formLogin(AbstractHttpConfigurer::disable);
+
+        // logout disable
+        http.logout(AbstractHttpConfigurer::disable);
+
+        // csrf disable
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        // session management
+        http.sessionManagement(session -> session
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션 미사용
+        );
+
+        return http.build();
+    }
+
+}
