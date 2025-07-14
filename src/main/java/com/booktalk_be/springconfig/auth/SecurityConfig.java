a74b64d04cad0a44788d476f15bd72339835a8dd
이성종 <dltjdwhd1235@naver.com>
2025-07-14 21:11:03 +0900

    gathering permitall

--- src/main/java/com/booktalk_be/springconfig/auth/SecurityConfig.java
+++ src/main/java/com/booktalk_be/springconfig/auth/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig  {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/member/**", "/dashboard/**", "/token-refresh", "/error").permitAll()
+                        .requestMatchers("/login","/gathering/**", "/member/**", "/dashboard/**", "/token-refresh", "/error").permitAll()
                         .anyRequest().authenticated()
                 )
                 .csrf(AbstractHttpConfigurer::disable)
